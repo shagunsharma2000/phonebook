151b5849ef414dd8dd59fa7df4ce13063a491147
shagunsharma2000 <148771257+shagunsharma2000@users.noreply.github.com>
2024-03-13 15:02:31 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# phonebook
+# phoneBook is my  Project and the folder name is PHONESQL
+
+This is my project. It is a Node.js project that creates a README file.
+
+## Installation
+
+To install the project, run the following command:
+ npm - node package manager  is the full form.
+ Installing their package in the file start NPM.
+ npm init -y  to install package.json 
+ npm i  to install node_modules
+ npm i express , bcrypt , dotenv , sequelize ,sql , mysql2 , body-parser install All Package to the file.
+ After that npm i nodemon  to  install their package to run automatically to server in a file in node.js.
+ "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "nodemon index.js"
+  },
+
+## All packages installed list 
+"scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "nodemon index.js"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "bcrypt": "^5.1.1",
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.4.5",
+    "express": "^4.18.2",
+    "mysql2": "^3.9.2",
+    "nodemon": "^3.1.0",
+    "sequelize": "^6.37.1",
+    "sql": "^0.78.0"
+  }
+
+# In file change  the code to run the command  npm start their are automatically refresh their server.
+
---
+++ index.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import connect from './src/dbconfig/dbconfig.js';
+import routes from './src/routes/index.js';
+import Sequelize from 'sequelize';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Connect to MySQL
+ const connection = connect();
+ connection.connect((err) => {
+   if (err) {
+     console.error("Error connecting to MySQL:", err);
+     return;
+   }
+   console.log("Connected to MySQL!");
+});
+
+
+// Create phones table
+const createPhonesTableQuery = `
+CREATE TABLE IF NOT EXISTS phones (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    mobileno VARCHAR(10),
+    userid INT,
+    FOREIGN KEY (userid) REFERENCES users(id),
+    isActive BOOLEAN DEFAULT true,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )
+`;
+
+connection.query(createPhonesTableQuery, (err, result) => {
+    if (err) {
+        console.error("Error creating phones table:", err);
+        return;
+    }
+    console.log("phones table created successfully!");
+});
+
+
+// Create users table
+const createUsersTableQuery =`
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255),
+    password VARCHAR(255),
+    email VARCHAR(255)
+    isActive BOOLEAN DEFAULT true,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+`;
+
+connection.query(createUsersTableQuery, (err, result) => {
+    if (err) {
+        console.error("Error creating users table:", err);
+        return;
+    }
+    console.log("Users table created successfully!");
+
+    // Close the connection
+    connection.end();
+});
+
+
+// Attach routes
+app.use('/api/v1', routes);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
